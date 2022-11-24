70325353d20802a5cbccd5b11125eef0afa9a77e
berkayucerr <berkayucerr@gmail.com>
2022-11-24 18:28:50 +0300

    Initial Commit

---
+++ Haberler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Haberler
+//
+//  Created by Berkay ÜÇER on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
